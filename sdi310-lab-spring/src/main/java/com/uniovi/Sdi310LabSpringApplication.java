28ffbf4152424fd8d7db05d1939fa1151b65160b
Gema Rico Pozas <UO238096@uniovi.es>
2019-02-14 11:27:01 +0100

    Creación inicial del proyecto Notaneitor

---
+++ sdi310-lab-spring/src/main/java/com/uniovi/Sdi310LabSpringApplication.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi310LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi310LabSpringApplication.class, args);
+	}
+
+}
+
