1fd2bfdaab526e0b92551433e056117abc50f018
Gema Rico Pozas <UO238096@uniovi.es>
2019-02-22 17:58:25 +0100

    SDI-SpringBoot-Sesión-4.5-Búsqueda

--- sdi310-lab-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
+++ sdi310-lab-spring/src/main/java/com/uniovi/repositories/MarksRepository.java
@@ -2,11 +2,9 @@ package com.uniovi.repositories;
 
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 
@@ -15,11 +13,16 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
 	@Modifying
 	@Transactional
 	@Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
-	void updateResend(Boolean resend, Long id);
-	
+	void updateResend(Boolean resend, Long id);	
 	
 	@Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC ")
 	List<Mark> findAllByUser(User user);
+	
+	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+	List<Mark> searchByDescriptionAndName(String seachtext);
+	
+	@Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2 ")
+	List<Mark> searchByDescriptionNameAndUser(String seachtext, User user);
 
 
 }
